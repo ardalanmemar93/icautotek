903d585d6521a7a1e1fb65a2940595d7d6fe7305
Marjan <97312708+Marjanamiiri@users.noreply.github.com>
2024-03-14 10:15:44 -0700

    navbar done

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li><a href="/servicesPrices">Services & Prices</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/gallery">Gallery</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
